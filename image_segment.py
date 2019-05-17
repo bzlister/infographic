0afbaa6e89b8b22dc5682f332798f444cd69c1aa
bzlister <bzlister@gmail.com>
2019-05-17 19:04:43 -0400

    image segmentation working properly

--- image_segment.py
+++ image_segment.py
@@ -9,64 +9,60 @@ from skimage import data
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io
-
+from skimage.measure import label, regionprops
+import math
+import cv2
 
 def image_show(split_image):
     plt.imshow(split_image)
     plt.show()
 
 image = io.imread('images\\ig1.png')
-transparent_px = [0,0,0]
+transparent_px = np.uint8([0,0,0,0])
 image_slic = seg.slic(image,n_segments=6) #need to tune this parameter for every image
-borders = seg.find_boundaries(image_slic)
+regions = regionprops(image_slic)
+leftover = cv2.cvtColor(image.copy(), cv2.COLOR_RGB2RGBA)
+for props in regions:
+    pos = props.filled_image
+    minr, minc, maxr, maxc = props.bbox
+    clip = image[minr:maxr,minc:maxc].copy()
+    clip = cv2.cvtColor(clip, cv2.COLOR_RGB2RGBA)
+    for ii in range(0, len(clip)):
+        for jj in range(0, len(clip[ii])):
+            if (pos[ii][jj] == False):
+                clip[ii][jj] = transparent_px
+            else:
+                leftover[ii+minr][jj+minc] = transparent_px
+    image_show(clip)
 
-#Partition image into sub-images based on border
-i = 0
-sub_images = []
-sub_image = []
-offset = 0
-np.append(borders, len(borders[-1])*[True])
-while (i < len(borders)):
-    sub_image.append([])
-    j = 0
-    while (j < len(borders[i])):
-        if (borders[i][j] == False):
-            if (j > len(sub_image[i-offset])):
-                sub_image[i-offset].append([transparent_px]*(j-len(sub_image[i-offset]))) #left-pad with null values
-            sub_image[i-offset].append(image[i][j].tolist())
-            j += 1
-        else:
+delcount =0
+l = len(leftover)
+r = 0
+while (r < l):
+    trans = True
+    for px in leftover[r]:
+        if (px[3] != 0):
+            trans = False
             break
-    if (j == 0):
-        sub_images.append(sub_image[:-1])
-        sub_image = []
-        offset = i+1
-    i += 1
-sub_images.append(sub_image)
-
-#Right-pad with null values
-for img in sub_images:
-    cluster = []
-    longest = 0
-    for row in img:
-        if (len(row) > longest):
-            longest = len(row)
-    for r in img:
-        if (r != []):
-            try:
-                cluster.append(r + [transparent_px]*(longest-len(r)))
-            except:
-                cluster.append([r] + [transparent_px]*(longest-len(r)))
-    if (longest != 0):
-        im3d = np.empty([len(cluster), len(cluster[0]), 3])   
-        for u in range(0, len(cluster)):
-            for v in range(0, len(cluster[u])):
-                try:
-                    im3d[u][v] = cluster[u][v]
-                except:
-                    print(cluster[u])
-                    x = 6/0
-        image_show(im3d)
+    if (trans):
+        leftover = np.delete(leftover, r, 0)
+        l = len(leftover)
+    else:
+        r += 1
+c = 0
+w = len(leftover[0])
+while (c < w):
+    trans = True
+    for row in range(0, len(leftover)):
+        if (leftover[row][c][3] != 0):
+            trans = False
+            break
+    if (trans):
+        leftover = np.delete(leftover, c, 1)
+        w = len(leftover[0])
+    else:
+        c += 1
+image_show(leftover)
+    
     #io.imsave("frag\\frag" + str(count) + ".png", im3d)
-    #count += 1
     #image_show(color.label2rgb(image_slic, image, kind='avg'))
